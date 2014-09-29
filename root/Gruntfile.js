1315a2bdbc20fb0b021b1ad6e424ea8bdda0d6a5
aallison <aallison@yahoo-inc.com>
2014-09-29 11:05:08 -0700

    updating loadConfig helper to support options files with multiple task configs

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -5,18 +5,29 @@ var timer = require("grunt-timer");
 
 function loadConfig(grunt, path) {
     var glob = require('glob');
-    var object = {};
-    var key;
+    var config = {};
 
-    glob.sync('*', { cwd: path }).forEach(function(option) {
-        key = option.replace(/\.js$/, '');
-        object[key] = require(path + option)(grunt);
+    glob.sync('*', {cwd: path}).forEach(function(option) {
+        grunt.log.debug("Loading config from: " + option);
+
+        var result = require(path + option)(grunt);
+        var key = option.replace(/\.js$/,'');
+
+        if (_.has(result, '_multiconfig')) {
+            _.each(result, function(value, innerKey) {
+                grunt.log.debug("  key: " + innerKey);
+                config[innerKey] = value;
+            });
+        } else {
+            grunt.log.debug("  key: " + key);
+            config[key] = result;
+        }
     });
 
-    return object;
+    return config;
 }
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     // Project configuration.
     require("load-grunt-tasks")(grunt);
@@ -27,7 +38,10 @@ module.exports = function(grunt) {
     // Load rest of tasks from ./grunt/tasks/*.js
     grunt.loadTasks('grunt/tasks');
 
+    // generates banner from info in package.json
     var getBanner = require("./grunt/helpers/bannerHelper")(grunt);
+
+    // our main app config file
     var pkg = grunt.file.readJSON('package.json');
 
     var config = {
